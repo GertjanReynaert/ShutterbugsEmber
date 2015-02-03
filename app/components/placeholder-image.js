7b34266984caf611b8304003a006e2c5f38e5764
Gertjan Reynaert <gertjan@typework.com>
2015-02-03 22:02:53 +0100

    add dynamic classes by setting style

--- app/components/placeholder-image.js
+++ app/components/placeholder-image.js
@@ -1,6 +1,22 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  tagName: 'img',
+
+  classNameBindings: [
+    'rounded:img-rounded',
+    'centered:center-block',
+    'circle:img-circle',
+    'thumbnail:img-thumbnail'
+  ],
+
+  attributeBindings: [
+    'w:width',
+    'h:height',
+    'url:src',
+    'alt'
+  ],
+
   xsmall: 20,
   small: 64,
   medium: 150,
@@ -15,10 +31,6 @@ export default Ember.Component.extend({
     //...
 
   // style
-    // dynamically add classes
-      // rounded
-      // centered
-      // circle
     // change ratio of width vs height
       // square
       // landscape
@@ -52,11 +64,47 @@ export default Ember.Component.extend({
     }
   }.property("height"),
 
-  url: function() {
-    return "data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iMTQwIiBoZWlnaHQ9IjE0MCIgdmlld0JveD0iMCAwIDE0MCAxNDAiIHByZXNlcnZlQXNwZWN0UmF0aW89Im5vbmUiPjxkZWZzLz48cmVjdCB3aWR0aD0iMTQwIiBoZWlnaHQ9IjE0MCIgZmlsbD0iI0VFRUVFRSIvPjxnPjx0ZXh0IHg9IjQ0LjA0Njg3NSIgeT0iNzAiIHN0eWxlPSJmaWxsOiNBQUFBQUE7Zm9udC13ZWlnaHQ6Ym9sZDtmb250LWZhbWlseTpBcmlhbCwgSGVsdmV0aWNhLCBPcGVuIFNhbnMsIHNhbnMtc2VyaWYsIG1vbm9zcGFjZTtmb250LXNpemU6MTBwdDtkb21pbmFudC1iYXNlbGluZTpjZW50cmFsIj4xNDB4MTQwPC90ZXh0PjwvZz48L3N2Zz4=";
-  }.property("type"),
-
   alt: function() {
     return "" + this.get("w") + "x" + this.get("h");
-  }.property("w", "h")
+  }.property("w", "h"),
+
+  rounded: function() {
+    var style = this.get("style") || "";
+    var keywords = style.split(" ");
+    keywords = keywords.map(function(keyword) {
+      return keyword.toLowerCase();
+    });
+    return keywords.indexOf("rounded") > -1;
+  }.property('style'),
+
+  centered: function() {
+    var style = this.get("style") || "";
+    var keywords = style.split(" ");
+    keywords = keywords.map(function(keyword) {
+      return keyword.toLowerCase();
+    });
+    return keywords.indexOf("center") > -1;
+  }.property('style'),
+
+  circle: function() {
+    var style = this.get("style") || "";
+    var keywords = style.split(" ");
+    keywords = keywords.map(function(keyword) {
+      return keyword.toLowerCase();
+    });
+    return keywords.indexOf("circle") > -1;
+  }.property('style'),
+
+  thumbnail: function() {
+    var style = this.get("style") || "";
+    var keywords = style.split(" ");
+    keywords = keywords.map(function(keyword) {
+      return keyword.toLowerCase();
+    });
+    return keywords.indexOf("thumbnail") > -1;
+  }.property('style'),
+
+  url: function() {
+    return "data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iMTQwIiBoZWlnaHQ9IjE0MCIgdmlld0JveD0iMCAwIDE0MCAxNDAiIHByZXNlcnZlQXNwZWN0UmF0aW89Im5vbmUiPjxkZWZzLz48cmVjdCB3aWR0aD0iMTQwIiBoZWlnaHQ9IjE0MCIgZmlsbD0iI0VFRUVFRSIvPjxnPjx0ZXh0IHg9IjQ0LjA0Njg3NSIgeT0iNzAiIHN0eWxlPSJmaWxsOiNBQUFBQUE7Zm9udC13ZWlnaHQ6Ym9sZDtmb250LWZhbWlseTpBcmlhbCwgSGVsdmV0aWNhLCBPcGVuIFNhbnMsIHNhbnMtc2VyaWYsIG1vbm9zcGFjZTtmb250LXNpemU6MTBwdDtkb21pbmFudC1iYXNlbGluZTpjZW50cmFsIj4xNDB4MTQwPC90ZXh0PjwvZz48L3N2Zz4=";
+  }.property("kind")
 });
