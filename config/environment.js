85ba6431c9da9ed0c05098236bd22ba1695fb2ea
Gertjan Reynaert <gertjan@typework.com>
2015-02-08 00:40:47 +0100

    fix csp to comply with added splashbase implementation

--- config/environment.js
+++ config/environment.js
@@ -27,8 +27,9 @@ module.exports = function(environment) {
     // ENV.APP.LOG_VIEW_LOOKUPS = true;
     ENV.contentSecurityPolicy = {
       'default-src': "'none'",
-      'img-src': "'self' https://s3.amazonaws.com https://ununsplash.imgix.net https://unsplash.imgix.net",
-      'connect-src': "'self' http://uifaces.com http://www.splashbase.co"
+      'img-src': "'self' https://s3.amazonaws.com https://*.s3.amazonaws.com https://ununsplash.imgix.net https://unsplash.imgix.net",
+      'connect-src': "'self' http://uifaces.com http://www.splashbase.co",
+      'script-src': "'self' https://jsonp.nodejitsu.com "
     };
   }
 
